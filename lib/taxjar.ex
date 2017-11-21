118241c32c84f402250ee41008df497f762f3239
Chris Yammine <me@chrisyammine.com>
2017-11-21 10:10:18 -0800

    Initial commit

---
+++ lib/taxjar.ex
@@ -0,0 +1,18 @@
+defmodule Taxjar do
+  @moduledoc """
+  Documentation for Taxjar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Taxjar.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/taxjar_test.exs
@@ -0,0 +1,8 @@
+defmodule TaxjarTest do
+  use ExUnit.Case
+  doctest Taxjar
+
+  test "greets the world" do
+    assert Taxjar.hello() == :world
+  end
+end
