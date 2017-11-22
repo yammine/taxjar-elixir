8069b64019c8e697f6b0ac10305d5425fd292a34
Chris Yammine <me@chrisyammine.com>
2017-11-21 17:52:38 -0800

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -6,8 +6,10 @@ defmodule Taxjar.Mixfile do
       app: :taxjar,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      desciption: "A client library for use of the TaxJar API.",
     ]
   end
 
@@ -22,7 +24,16 @@ defmodule Taxjar.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.13"},
-      {:poison, "~> 3.0"}
+      {:poison, "~> 3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["ChrisYammine"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/ChrisYammine/taxjar-elixir"}
     ]
   end
 end
